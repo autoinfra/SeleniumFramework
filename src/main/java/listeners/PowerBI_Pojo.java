5ff3a74f98aad1781fda495407483b4a7dfa4fef
Bhargav Murari <bhargavkumar.65@gmail.com>
2020-09-19 12:58:07 +0530

    Reducing BoilerPlate Code

--- src/main/java/listeners/PowerBI_Pojo.java
+++ src/main/java/listeners/PowerBI_Pojo.java
@@ -1,7 +1,9 @@
 package listeners;
 
-import java.util.List;
+import lombok.Builder;
 
+import java.util.List;
+@Builder
 public class PowerBI_Pojo {
     private String Suitename;
     private String Classname;
@@ -10,61 +12,5 @@ public class PowerBI_Pojo {
     private String Host;
     private String Result;
     private long Duration;
-
-    public void setSuitename(String suitename) {
-        Suitename = suitename;
-    }
-
-    public void setClassname(String classname) {
-        Classname = classname;
-    }
-
-    public void setMethodname(String methodname) {
-        Methodname = methodname;
-    }
-
-    public void setMethodDescription(String methodDescription) {
-        MethodDescription = methodDescription;
-    }
-
-    public void setHost(String host) {
-        Host = host;
-    }
-
-    public void setResult(String result) {
-        Result = result;
-    }
-
-    public String getSuitename() {
-        return Suitename;
-    }
-
-    public String getClassname() {
-        return Classname;
-    }
-
-    public String getMethodname() {
-        return Methodname;
-    }
-
-    public String getMethodDescription() {
-        return MethodDescription;
-    }
-
-    public String getHost() {
-        return Host;
-    }
-
-    public String getResult() {
-        return Result;
-    }
-
-    public long getDuration() {
-        return Duration;
-    }
-
-    public void setDuration(long duration) {
-        Duration = duration;
-    }
 
 }
