28f5a765c60c3d11a4d112979a5b3d9341f7d092
Bhargav Murari <bhargavkumar.65@gmail.com>
2020-09-20 11:45:09 +0530

    Disable WebdriverManager Scope as Test only

--- src/main/java/base/base_redefined.java
+++ src/main/java/base/base_redefined.java
@@ -1,8 +1,12 @@
 package base;
 
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
@@ -12,11 +16,8 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.openqa.selenium.WebDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.text.SimpleDateFormat;
